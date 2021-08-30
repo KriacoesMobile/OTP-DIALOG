0681e4f3c87a16968b3a0cbed324d0a61229299f
Kouki Badr <badreddinekouki1998@gmail.com>
2021-08-30 07:47:12 +0100

    create otp dialog base widget

--- lib/otp_dialog.dart
+++ lib/otp_dialog.dart
@@ -1,16 +1,31 @@
 import 'package:flutter/material.dart';
 
 class OTPDialog extends StatefulWidget {
-  
   @override
   _OTPDialogState createState() => _OTPDialogState();
+
+  show(context) {
+    return showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          content: this,
+          contentPadding: EdgeInsets.all(0),
+          
+        );
+      },
+    );
+  }
 }
 
 class _OTPDialogState extends State<OTPDialog> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      
+      height: 250,
+      width: 400,
+      decoration: BoxDecoration(
+          color: Colors.white, borderRadius: BorderRadius.circular(20)),
     );
   }
 }
