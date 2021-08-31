ff49395d00de4ebc1b78001988896c5e8199d68e
Kouki Badr <badreddinekouki1998@gmail.com>
2021-08-31 06:46:29 +0100

    create otp dialog layout commponents

--- lib/otp_dialog.dart
+++ lib/otp_dialog.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class OTPDialog extends StatefulWidget {
@@ -11,7 +12,6 @@ class OTPDialog extends StatefulWidget {
         return AlertDialog(
           content: this,
           contentPadding: EdgeInsets.all(0),
-          
         );
       },
     );
@@ -26,6 +26,56 @@ class _OTPDialogState extends State<OTPDialog> {
       width: 400,
       decoration: BoxDecoration(
           color: Colors.white, borderRadius: BorderRadius.circular(20)),
+      child: Padding(
+        padding: const EdgeInsets.all(5),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Authentification doube facteur",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                textAlign: TextAlign.center),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: _renderOTPInputFields(5),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text("Renvoyer le code",style: TextStyle(
+              decoration: TextDecoration.underline
+            ),)
+          ],
+        ),
+      ),
     );
+  }
+
+  _renderOTPInputFields(int inputFieldsCount) {
+    List<Widget> inputFields = [];
+    for (var i = 0; i < inputFieldsCount; i++) {
+      if (i != 0) {
+        inputFields.add(SizedBox(
+          width: 10,
+        ));
+      }
+      inputFields
+          .add(Container(width: 40, height: 40, decoration: BoxDecoration(
+            border: Border.all(
+              width: 1,
+              color: Colors.grey
+            )
+          ),child: CupertinoTextField()));
+    }
+    return inputFields;
   }
 }
